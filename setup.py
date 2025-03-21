d9f1dfb09bdb4640f29a02b9f8dac35bdacc7d03
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2025-03-21 10:51:01 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "trygzerodegreedayscities",
+    version = "0.0.1",
+    author = "Etienne Dunn-Sigouin",
+    packages=['trygzerodegreedayscities'],
+    install_requires=["numpy","xarray","scipy"],
+)
+
